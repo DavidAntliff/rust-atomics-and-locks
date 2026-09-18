3954f950931e497dcf0ef2132af4adb407f33430
David Antliff <david@pitchblende.co.nz>
2026-09-18 16:15:35 +1000

    Chapter 8.

---
+++ src/chapter8/mod.rs
@@ -0,0 +1,51 @@
+use std::sync::atomic::AtomicU32;
+use std::sync::atomic::Ordering::Relaxed;
+use std::thread;
+use std::time::Duration;
+
+#[cfg(not(target_os = "linux"))]
+compile_error!("Linux only. Sorry!");
+
+pub fn wait(a: &AtomicU32, expected: u32) {
+    // Refer to the futex (2) man page for the syscall signature.
+    unsafe {
+        libc::syscall(
+            libc::SYS_futex, // The futex syscall.
+            a as *const AtomicU32, // The atomic to operate on.
+            libc::FUTEX_WAIT, // The futex operation.
+            expected, // The expected value.
+            std::ptr::null::<libc::timespec>(), // No timeout.
+        );
+    }
+}
+
+pub fn wake_one(a: &AtomicU32) -> u32 {
+    // Refer to the futex (2) man page for the syscall signature.
+    unsafe {
+        libc::syscall(
+            libc::SYS_futex, // The futex syscall.
+            a as *const AtomicU32, // The atomic to operate on.
+            libc::FUTEX_WAKE, // The futex operation.
+            1, // The number of threads to wake up.
+        ) as u32
+    }
+}
+
+pub fn run_all() {
+    let a = AtomicU32::new(0);
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            thread::sleep(Duration::from_secs(3));
+            a.store(1, Relaxed);
+            let c = wake_one(&a);
+            println!("Woke up {} threads", c);
+        });
+
+        println!("Waiting...");
+        while a.load(Relaxed) == 0 {
+            wait(&a, 0);
+        }
+        println!("Done!");
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod chapter4;
 pub mod chapter5;
 pub mod chapter6;
 pub mod chapter7;
+pub mod chapter8;
 
 fn main() {
     //chapter1::run_all();
@@ -13,5 +14,6 @@ fn main() {
     //chapter4::run_all();
     //chapter5::run_all();
     //chapter6::run_all();
-    chapter7::run_all();
+    //chapter7::run_all();
+    chapter8::run_all();
 }
